8761c4f5c40817bd597c372e10d126633a2062eb
pyotoru <wawrzyniak.peter@gmail.com>
2021-04-14 00:11:49 +0900

    palindrome done

--- palindrome checker.js
+++ palindrome checker.js
@@ -8,3 +8,18 @@ We'll pass strings with varying formats, such as racecar, RaceCar, and race CAR 
 
 We'll also pass strings with special symbols, such as 2A3*3a2, 2A3 3a2, and 2_A3*3#A2.
 */
+
+function palindrome(str) {
+	var removeNonAlpha = str.replace(/\W|_/g, "");
+	var lowerCase = removeNonAlpha.toLowerCase(removeNonAlpha);
+	var reverse = [];
+	for (let i = lowerCase.length - 1; i >= 0; i--) {
+		reverse.push(lowerCase[i]);
+	}
+	var reverseJoin = reverse.join("");
+	if (lowerCase === reverseJoin) {
+		return true;
+	} else {
+		return false;
+	}
+}
---
+++ roman numeral converter.js
@@ -0,0 +1,10 @@
+/*Convert the given number into a roman numeral.
+
+All roman numerals answers should be provided in upper-case.*/
+
+function convertToRoman(num) {
+	var numbers = [1, 4, 5, 10, 40, 50, 90, 100, 900, 1000];
+	var romanNumerals = [I, IV, V, X, XL, L, LC, C, CD, D, M];
+	return num;
+}
+convertToRoman(36);
