4954572c81013a830bc76761b6cf98e3aa936f29
pyotoru <wawrzyniak.peter@gmail.com>
2021-04-17 23:00:36 +0900

    update

--- Projects/palindrome-checker.js
+++ Projects/palindrome-checker.js
@@ -22,3 +22,16 @@ function palindrome(str) {
 		return false;
 	}
 }
+console.log(palindrome("eye"));
+console.log(palindrome("eye"));
+console.log(palindrome("_eye"));
+console.log(palindrome("race car"));
+console.log(palindrome("not a palindrome"));
+console.log(palindrome("A man, a plan, a canal. Panama"));
+console.log(palindrome("never odd or even"));
+console.log(palindrome("nope"));
+console.log(palindrome("almostomla"));
+console.log(palindrome("My age is 0, 0 si ega ym."));
+console.log(palindrome("1 eye for of 1 eye."));
+console.log(palindrome("0_0 (: /- :) 0-0"));
+console.log(palindrome("five|_/|four"));
